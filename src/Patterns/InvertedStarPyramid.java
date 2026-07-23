d29ea13f10031ec146e02347059d7874828aeb43
Sagar Yadav <sagaryadav6849@gmail.com>
2026-07-23 17:16:08 +0530

    Inverted Star Pyramid Pattern

---
+++ src/Patterns/InvertedStarPyramid.java
@@ -0,0 +1,28 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class InvertedStarPyramid {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        pattern8(n);
+    }
+    static void pattern8(int n){
+        for (int i = 0; i < n; i++) {
+            // for spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            // for star
+            for (int j = 0; j < (2*n-(2*i+1)); j++) {
+                System.out.print("*");
+            }
+            // for spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
