d4d07198d720135c7ab7cca6ad382008cf2def6c
Sagar Yadav <sagaryadav6849@gmail.com>
2026-07-25 21:15:02 +0530

    Half Diamond Star Pattern Updated

--- src/Patterns/HalfDiamondStarPattern.java
+++ src/Patterns/HalfDiamondStarPattern.java
@@ -9,6 +9,19 @@ public class HalfDiamondStarPattern {
         pattern10(n);
     }
     static void pattern10(int n){
-
+        // 1st half
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // 2nd half
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <=n-i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
