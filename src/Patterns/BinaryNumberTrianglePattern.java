676d17f65f7b645f1dda1007600cf4deb1216681
Sagar Yadav <sagaryadav6849@gmail.com>
2026-07-26 21:56:14 +0530

    Binary Number Triangle pattern prototype

---
+++ src/Patterns/BinaryNumberTrianglePattern.java
@@ -0,0 +1,15 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class BinaryNumberTrianglePattern {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        pattern11(n);
+    }
+    static void pattern11(int n){
+
+
+    }
+}
