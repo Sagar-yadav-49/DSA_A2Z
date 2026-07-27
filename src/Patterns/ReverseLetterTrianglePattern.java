b82aee542473a5d165383b93ba83dd895a439fff
Sagar Yadav <sagaryadav6849@gmail.com>
2026-07-27 19:55:34 +0530

    Reverse Letter Triangle Pattern

---
+++ src/Patterns/ReverseLetterTrianglePattern.java
@@ -0,0 +1,20 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class ReverseLetterTrianglePattern {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n=input.nextInt();
+        pattern15(n);
+    }
+    static void pattern15(int n){
+        for (int i = 0; i < n; i++) {
+            char count=(char) 65;
+            for (int j = 0; j <(n-i) ; j++) {
+                System.out.print(count++);
+            }
+            System.out.println();
+        }
+    }
+}
