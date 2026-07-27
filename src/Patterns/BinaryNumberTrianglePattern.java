f780d44106abdabec8ed787eda82516c3fff5477
Sagar Yadav <sagaryadav6849@gmail.com>
2026-07-27 12:48:52 +0530

    Binary Number Triangle Pattern Code

--- src/Patterns/BinaryNumberTrianglePattern.java
+++ src/Patterns/BinaryNumberTrianglePattern.java
@@ -9,7 +9,19 @@ public class BinaryNumberTrianglePattern {
         pattern11(n);
     }
     static void pattern11(int n){
-
+        int start=1;
+        for (int i = 0; i < n; i++) {
+            if(i%2==0){
+                start=1;
+            } else{
+                start=0;
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(start+" ");
+                start=1-start;
+            }
+            System.out.println();
+        }
 
     }
 }
