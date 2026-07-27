2e2035c35631817104827125c4a33e40f3523575
Sagar Yadav <sagaryadav6849@gmail.com>
2026-07-27 20:01:58 +0530

    Alpha-Ramp Pattern

---
+++ src/Patterns/AlphaRampPattern.java
@@ -0,0 +1,21 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class AlphaRampPattern {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        pattern16(n);
+    }
+    static void pattern16(int n){
+        char count=(char) 65;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <=i ; j++) {
+                System.out.print(count);
+            }
+            System.out.println();
+            count++;
+        }
+    }
+}
