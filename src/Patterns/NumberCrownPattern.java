03b2961d046bbdfca15519397b1c24ee6676a690
Sagar Yadav <sagaryadav6849@gmail.com>
2026-07-27 13:21:02 +0530

    Number Crown Pattern

---
+++ src/Patterns/NumberCrownPattern.java
@@ -0,0 +1,31 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class NumberCrownPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pattern12(n);
+    }
+    static void pattern12(int n){
+        for (int i = 0; i < n; i++) {
+            // for number
+            int count1=1;
+            for (int j = 0; j <=i ; j++) {
+                System.out.print(count1++);
+            }
+            // for spaces
+            for (int j = 0; j < ((2*n)-((2*i)+2)); j++) {
+                System.out.print(" ");
+            }
+            // for number
+            int count2=i+1;
+            for (int j = 0; j <=i ; j++) {
+                System.out.print(count2--);
+            }
+
+            System.out.println();
+        }
+    }
+}
