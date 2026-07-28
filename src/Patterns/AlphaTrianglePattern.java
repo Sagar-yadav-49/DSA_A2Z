81f7844dc2f2c69b23c709542b8e954e5a911923
Sagar Yadav <sagaryadav6849@gmail.com>
2026-07-28 22:02:14 +0530

    Alpha Triangle Pattern Prototype

---
+++ src/Patterns/AlphaTrianglePattern.java
@@ -0,0 +1,13 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class AlphaTrianglePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pattern18(n);
+    }
+    static void pattern18(int n){
+    }
+}
