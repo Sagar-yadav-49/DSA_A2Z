d0d95dee1e200ec054e73f8d237d0bb69daa6cba
Sagar Yadav <sagaryadav6849@gmail.com>
2026-07-29 14:53:12 +0530

    Alpha Triangle Pattern Code

--- src/Patterns/AlphaTrianglePattern.java
+++ src/Patterns/AlphaTrianglePattern.java
@@ -9,5 +9,14 @@ public class AlphaTrianglePattern {
         pattern18(n);
     }
     static void pattern18(int n){
+        char count=(char)(65+n-1);
+        for (int i = 0; i < n; i++) {
+            int var=count-i;
+            for (int j = 0; j <=i ; j++) {
+                System.out.print((char)var+" ");
+                var++;
+            }
+            System.out.println();
+        }
     }
 }
