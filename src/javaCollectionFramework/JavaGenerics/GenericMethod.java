242251759c9902e8c9b162d2710d8aa7cf63791a
Sagar Yadav <sagaryadav6849@gmail.com>
2026-08-01 21:40:45 +0530

    Generic Method prototype

---
+++ src/javaCollectionFramework/JavaGenerics/GenericMethod.java
@@ -0,0 +1,17 @@
+package javaCollectionFramework.JavaGenerics;
+
+public class GenericMethod {
+    public static void main(String[] args) {
+        printData("sagar");
+        printData(123);
+    }
+//    static void printData(String data){
+//        System.out.println(data);
+//    }
+
+    static <T> void printData(T data){
+        System.out.println(data);
+    }
+
+
+}
