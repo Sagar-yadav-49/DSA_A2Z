b7cea0740a2403c4177def67cf84dd7f10aad6a5
Sagar Yadav <sagaryadav6849@gmail.com>
2026-08-09 13:16:11 +0530

    understanding list

---
+++ src/javaCollectionFramework/ListAndCollectionFramework/LearnList.java
@@ -0,0 +1,48 @@
+package javaCollectionFramework.ListAndCollectionFramework;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ArrayList;
+
+public class LearnList {
+    public static void main(String[] args) {
+        // ArrayList is dynamic in size
+        List<Integer> list=new ArrayList<>();
+        list.add(10);
+        list.add(34);
+        list.add(21);
+
+        System.out.println(list);
+
+        System.out.println(list.size());
+
+        System.out.println(list.isEmpty());
+
+        System.out.println(list.contains(34));
+
+        //
+        list.remove(1);
+        System.out.println(list);
+
+        //
+        list.remove(Integer.valueOf(10));
+        System.out.println(list);
+
+        List<Integer> list2=new ArrayList<>();
+        list2.add(100);
+        list2.add(200);
+        list2.add(300);
+
+        list.addAll(list2);
+        System.out.println(list);
+
+        list.removeAll(list2);
+        System.out.println(list);
+
+        System.out.println();
+
+
+        // LinkedList
+        List<Integer> newList=new LinkedList<>(); // internal working changed
+    }
+}
