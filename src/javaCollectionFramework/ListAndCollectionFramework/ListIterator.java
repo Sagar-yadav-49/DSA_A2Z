ca725c1ce8075590959ea902e879d04f9108adb4
Sagar Yadav <sagaryadav6849@gmail.com>
2026-08-09 13:17:10 +0530

    Understanding Iterator and List Iterator

---
+++ src/javaCollectionFramework/ListAndCollectionFramework/ListIterator.java
@@ -0,0 +1,37 @@
+package javaCollectionFramework.ListAndCollectionFramework;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ListIterator {
+    public static void main(String[] args) {
+        List<String> fruits =new ArrayList<>();
+        fruits.add("apple");
+        fruits.add("banana");
+        fruits.add("kiwi");
+        fruits.add("pineapple");
+        // different ways to iterate over list
+        //
+        for (int i = 0; i < fruits.size(); i++) {
+            System.out.println("Fruit is: "+ fruits.get(i));
+        }
+        //
+        for(String ele:fruits){
+            System.out.println("Fruit is: "+ele);
+        }
+        // Iterator
+        Iterator<String> fe= fruits.iterator();
+        while(fe.hasNext()){
+            System.out.println(fe.next());
+        }
+
+        // sublist() method
+        List<String> subList=fruits.subList(1,3);
+        System.out.println(subList);
+
+
+
+    }
+}
+
