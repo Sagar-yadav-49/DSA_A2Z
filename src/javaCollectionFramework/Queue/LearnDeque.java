2f0a240f4097e427954920213f6d3085fe941a72
Sagar Yadav <sagaryadav6849@gmail.com>
2026-08-12 14:34:54 +0530

    Learning about ArrayDeque class

---
+++ src/javaCollectionFramework/Queue/LearnDeque.java
@@ -0,0 +1,28 @@
+package javaCollectionFramework.Queue;
+
+import java.util.ArrayDeque;
+
+public class LearnDeque {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> dq=new ArrayDeque<>();
+        dq.offer(10);
+
+        dq.offerLast(20);
+        dq.offerFirst(30);
+        dq.offerFirst(40);
+        dq.offerFirst(50);
+        System.out.println(dq);
+
+        // remove element
+        System.out.println(dq.poll());
+        System.out.println(dq.pollLast());
+        System.out.println(dq.pollFirst());
+        System.out.println(dq);
+        // viewing element
+        System.out.println(dq.peek());
+        System.out.println(dq.peekFirst());
+        System.out.println(dq.peekLast());
+
+
+    }
+}
